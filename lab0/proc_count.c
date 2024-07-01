d757d1cb9aa2768c59497daaacbf8316a17eb9d9
Kevin Xue <kevinxue331@gmail.com>
2024-07-01 15:05:32 -0500

    1

--- lab0/proc_count.c
+++ lab0/proc_count.c
@@ -7,7 +7,9 @@
 static struct proc_dir_entry *entry;
 
 static int proc_count(struct seq_file *m, void *v){
-	// TODO: it's all yours
+	int count =0;
+	for_each_process(p) count++;
+	seq_printf(m, "%d\n", count);
 	return 0;
 }
 
